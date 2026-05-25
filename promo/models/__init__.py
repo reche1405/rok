6e86d9c1c96250df4835e930da81ea4f0e3fb2b4
Reche Maynard <reche1405@hotmail.com>
2026-05-25 15:44:23 +0100

    Added sqlite WAL instructions.

--- promo/models/__init__.py
+++ promo/models/__init__.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import event
 db = SQLAlchemy()
+
+@event.listens_for(db.engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.close()
