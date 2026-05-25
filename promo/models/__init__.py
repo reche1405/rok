b587f4e9132c50218bf63c1648bce2da301b3eb3
Reche Maynard <reche1405@hotmail.com>
2026-05-25 16:21:15 +0100

    Changed the event listener when checking for the database connection.

--- promo/models/__init__.py
+++ promo/models/__init__.py
@@ -1,9 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import event
+from sqlalchemy.engine import Engine 
+
 db = SQLAlchemy()
 
-@event.listens_for(db.engine, "connect")
+@event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA journal_mode=WAL")
-    cursor.close()
+    if dbapi_connection.__class__.__name__ == 'SQLite3Connection':
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA journal_mode=WAL")
+        cursor.close()
