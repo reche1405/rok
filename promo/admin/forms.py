b4b387d990129e8e6401b36e37dfa1ec7bfcfae4
Reche Maynard <reche1405@hotmail.com>
2026-05-30 22:17:52 +0100

    Added a thumbnail to the media admin view.

--- promo/admin/forms.py
+++ promo/admin/forms.py
@@ -33,7 +33,7 @@ class ImagePreviewWidget(FileUploadInput):
         html = super(ImagePreviewWidget, self).__call__(field, **kwargs)
         if field.data:
             # Adjust "/static/uploads/" to your setup
-            preview_html = f'<div style="margin-bottom: 10px;"><img src="/static/uploads/{field.data}" style="max-height: 150px; border: 1px solid #ddd; padding: 5px; border-radius: 4px;"></div>'
+            preview_html = f'<div style="margin-bottom: 10px;"><img src="/media/{field.data}" style="max-height: 150px; border: 1px solid #ddd; padding: 5px; border-radius: 4px;"></div>'
             html = Markup(preview_html) + html
         return html
 
