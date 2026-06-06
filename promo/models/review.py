0c4744d442a395afc8ebb34c0e7a5f03a18a0eda
Reche Maynard <reche1405@hotmail.com>
2026-06-06 13:49:35 +0100

    Added in site reviews v0.2

--- promo/models/review.py
+++ promo/models/review.py
@@ -4,7 +4,7 @@ class Review(db.Model):
     __tablename__="reviews"
 id = db.Column(db.Integer, primary_key=True)
 rating = db.Column(db.Integer, nullable=False)
-project_id = db.Column(db.Integer, db.ForignKey('projects.id'), nullable=True)
-project = db.relationship('Project', backref=db.backref('projects', lazy=True))
+project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable=True)
+project = db.relationship('Project', backref=db.backref('reviews', lazy=True))
 name = db.Column(db.String(255), nullable=True)
 text = db.Column(db.Text, nullable=False)
